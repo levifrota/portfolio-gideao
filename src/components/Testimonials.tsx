a15f163568113a6dc69e4696aaa2c94ba7db9b6a
Gideao Levi <gideao.frota@stilingue.com.br>
2024-03-14 19:32:31 -0300

    feat: create pojects, services, skills and testimonials sections

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,8 +1,32 @@
+import { FaQuoteLeft } from 'react-icons/fa';
+
 export function Testimonials() {
   return (
-    <main>
-      <div>Testimonials text</div>
-    </main>
+    <section className="container mx-auto max-w-4xl p-4 py-8">
+      <div className="relative mb-4 p-4 text-center">
+        <h2 className="relative z-50 mb-2 font-bold">
+          <span className="mr-2 font-headline text-3xl text-gray-800">
+            Depoimentos de
+          </span>
+          <span className="font-handwriting text-4xl text-blue-800">
+            Clientes
+          </span>
+        </h2>
+        <div className="absolute left-1/2 top-3 z-0 h-10 w-10 rounded-lg bg-blue-400/10" />
+      </div>
+
+      <figure>
+        <FaQuoteLeft className='h-8 w-8 text-gray-600' />
+        <blockquote className="mb-6">
+          <p className="text-2xl text-gray-900">
+            Depoimento
+          </p>
+        </blockquote>
+        <figcaption className="flex items-center justify-center gap-2">
+          img
+        </figcaption>
+      </figure>
+    </section>
   );
 }
 
